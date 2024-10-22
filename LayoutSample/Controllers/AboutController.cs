53cdcbebf25962d512c4ffc3f74c5a530156e398
Barış Emre Yalçın <barisemre.bey@gmail.com>
2024-10-22 12:48:52 +0300

    initial commit

---
+++ LayoutSample/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutSample.Controllers
+{
+	public class AboutController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult BoardOfDirectors()
+		{
+			return View();
+		}
+
+		public IActionResult History()
+		{
+			return View();
+		}
+	}
+}
---
+++ LayoutSample/Controllers/NewsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutSample.Controllers
+{
+	public class NewsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult TrumpAssassination()
+		{
+			return View();
+		}
+
+		public IActionResult MarketDownturn()
+		{
+			return View();
+		}
+	}
+}
